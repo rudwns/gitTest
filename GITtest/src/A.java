69a19a25fb56b4f0143ba427243d463fb79fb31e
jackiehoon <wognsdl4545@naver.com>
2017-10-18 12:20:22 +0900

    ghghghgh

--- GITtest/src/A.java
+++ GITtest/src/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("DDDDDDDDDDDD");
+		System.out.println("121321321213");
 	}
 
 }
